28182fff7f789b04ab14b1c62656028417e5e120
Atharv Kachare <104071489+Atharvkachare@users.noreply.github.com>
2024-11-16 08:36:44 +0530

    Time: 1 ms (99.15%), Space: 44.7 MB (86.1%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,18 +1,22 @@
 class Solution {
-    public int jump(int[] nums) {
-        int n = nums.length;
-        int jumps = 0;
-        int current = 0;
-        int farthest = 0;
+  public int jump(int[] nums) {
+    int ans = 0;
+    int end = 0;
+    int farthest = 0;
 
-        for (int i = 0; i < n - 1; i++) {
-            farthest = Math.max(farthest, i + nums[i]);
-
-            if (i == current) {
-                jumps++;
-                current = farthest;
-            }
-        }
-        return jumps;
+    // Start an implicit BFS.
+    for (int i = 0; i < nums.length - 1; ++i) {
+      farthest = Math.max(farthest, i + nums[i]);
+      if (farthest >= nums.length - 1) {
+        ++ans;
+        break;
+      }
+      if (i == end) {   // Visited all the items on the current level.
+        ++ans;          // Increment the level.
+        end = farthest; // Make the queue size for the next level.
+      }
     }
+
+    return ans;
+  }
 }
