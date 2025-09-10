52915e54c166a8b2311bbf5dcc072a350b40aad4
Atharv Kachare <104071489+Atharvkachare@users.noreply.github.com>
2025-09-10 20:48:02 +0530

    Time: 0 ms (100%), Space: 44.9 MB (85.12%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int o = image[sr][sc];
+        helper(image, sr, sc, newColor, o);
+        return image;
+    }
+
+    private void helper(int[][] img, int r, int c, int n, int o) {
+        if (r >= img.length
+                || c >= img[0].length
+                || r < 0
+                || c < 0
+                || img[r][c] == n
+                || img[r][c] != o) {
+            return;
+        }
+        img[r][c] = n;
+        helper(img, r + 1, c, n, o);
+        helper(img, r - 1, c, n, o);
+        helper(img, r, c + 1, n, o);
+        helper(img, r, c - 1, n, o);
+    }
+}
